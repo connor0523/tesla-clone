5d77a27a00620aa1803ba003fcf8aaae76d700b5
Connor <connorce0523@gmail.com>
2022-11-10 11:40:43 -0500

    finished tesla-clone

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,11 @@ import React, { useState } from 'react'
 import styled from 'styled-components'
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
-import { selectCars } from "../components/features/car/carSlice"
-import { useSelector } from "react-redux"
+
 
 function Header() {
     const [burgerStatus, setBurgerStatus] = useState(false)
-    const cars = useSelector(selectCars)
-    console.log(cars)
+    
 
   return (
     
@@ -23,22 +21,29 @@ function Header() {
              <a href="#">Model Y</a>
         </Menu>
         <RightMenu>
-            <a href="#">Shop</a>
-            <a href="#">Account</a>
+            <a href="https://shop.tesla.com/" target="_blank">Shop</a>
+            <a href="https://auth.tesla.com/oauth2/v1/authorize?redirect_uri=https%3A%2F%2Fwww.tesla.com%2Fteslaaccount%2Fowner-xp%2Fauth%2Fcallback&response_type=code&client_id=ownership&scope=offline_access%20openid%20ou_code%20email%20phone&audience=https%3A%2F%2Fownership.tesla.com%2F&locale=en-US" target="_blank">Account</a>
             <CustomMenu onClick={() => setBurgerStatus(true)} />
         </RightMenu>
         <BurgerNav show={burgerStatus}>
             <CloseWrapper>
                 <CustomClose onClick={() => setBurgerStatus(false)} />
             </CloseWrapper>
-            <li><a href="">Existing Inventory</a></li>
-            <li><a href="">Used Inventory</a></li>
-            <li><a href="">Trade In</a></li>
-            <li><a href="">CyberTruck</a></li>
-            <li><a href="">Roadster</a></li>
-            <li><a href="">Existing Inventory</a></li>
-            <li><a href="">Existing Inventory</a></li>
-            <li><a href="">Existing Inventory</a></li>
+            <li><a href="https://www.tesla.com/inventory/new/m3?arrangeby=relevance&zip=28079&range=200" target="_blank">Existing Inventory</a></li>
+            <li><a href="https://www.tesla.com/inventory/used/m3?arrangeby=plh&zip=28079" target="_blank">Used Inventory</a></li>
+            <li><a href="https://auth.tesla.com/oauth2/v1/authorize?client_id=teslaweb&response_type=code&scope=openid%20email%20ou_code%20profile&redirect_uri=https%3A//www.tesla.com/openid-connect/generic&state=k-XWAaaRDMcVi5YHdBLVnqQZVOvJYLx1j05pcfdK5Gg&locale=en-US" target="_blank">Trade In</a></li>
+            <li><a href="https://www.tesla.com/drive" target="_blank">Test Drive</a></li>
+            <li><a href="https://www.tesla.com/insurance" target="_blank">Insurance</a></li>
+            <li><a href="https://www.tesla.com/cybertruck" target="_blank">Cybertruck</a></li>
+            <li><a href="https://www.tesla.com/roadster" target="_blank">Roadster</a></li>
+            <li><a href="https://www.tesla.com/semi" target="_blank">Semi</a></li>
+            <li><a href="https://www.tesla.com/charging" target="_blank">Charging</a></li>
+            <li><a href="https://www.tesla.com/powerwall" target="_blank">Powerwall</a></li>
+            <li><a href="https://www.tesla.com/commercial" target="_blank">Commercial Energy</a></li>
+            <li><a href="https://www.tesla.com/utilities" target="_blank">Utilities</a></li>
+            <li><a href="https://www.tesla.com/findus?v=2&bounds=54.88795900215465%2C-57.079102750000004%2C21.020893216720236%2C-141.45410275&zoom=5&filters=store%2Cservice%2Csupercharger%2Cdestination%20charger%2Cbodyshop%2Cparty" target="_blank">Find Us</a></li>
+            <li><a href="https://www.tesla.com/support" target="_blank">Support</a></li>
+            <li><a href="https://ir.tesla.com/#quarterly-disclosure" target="_blank">Investor Relations</a></li>
         </BurgerNav>
     </Container>
     
@@ -82,6 +87,7 @@ const Menu = styled.div`
 const RightMenu = styled.div`
     display: flex;
     align-items: center;
+    
     a {
         font-weight: 600;
         text-transform: uppercase;
